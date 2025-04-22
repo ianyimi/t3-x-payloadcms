29292c7c52483df01c9452a11a68ef9fa17c224d
yonko-zaye <isaiah@edaptschools.com>
2025-04-21 21:54:14 -0700

    init: scaffold payload cms admin into nextjs t3 app with mongodb

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.mjs";
+import { withPayload } from "@payloadcms/next/withPayload";
+
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
+};
+
+export default withPayload(nextConfig);
