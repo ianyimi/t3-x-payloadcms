2744ccdf304e85d12b2b55aef2f5cbdb8a707466
Isaiah Anyimi <41877924+ianyimi@users.noreply.github.com>
2025-07-19 13:00:31 -0700

    feat: change login to use google instead of email password link like the other endpoints. todo: style up the other endpoints so they look nicer or check the install instructions again for that component library for betterAuth

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -1,8 +1,7 @@
 "use client"
 
-import Link from 'next/link'
 import { Button } from '~/ui/button'
-import { useSession } from '~/auth/client'
+import { useSession, authClient } from '~/auth/client'
 import { useEffect } from 'react'
 
 export default function SignInButton() {
@@ -16,19 +15,17 @@ export default function SignInButton() {
 	}, [data?.session])
 
 	return (
-		<Link href={data?.session ? '/' : '/auth/sign-in'} className="flex justify-center">
-			<Button className="justify-between cursor-pointer gap-2" variant="default">
-				{!data?.session ? (
-					<svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24">
-						<path fill="currentColor" d="M5 3H3v4h2V5h14v14H5v-2H3v4h18V3zm12 8h-2V9h-2V7h-2v2h2v2H3v2h10v2h-2v2h2v-2h2v-2h2z"></path>
-					</svg>
-				) : (
-					<svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24">
-						<path fill="currentColor" d="M2 3h20v18H2zm18 16V7H4v12z"></path>
-					</svg>
-				)}
-				<span>{data?.session ? 'Dashboard' : 'Sign In'}</span>
-			</Button>
-		</Link>
+		<Button className="justify-between cursor-pointer gap-2" variant="default" onClick={() => authClient.signIn.social({ provider: 'google' })}>
+			{!data?.session ? (
+				<svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24">
+					<path fill="currentColor" d="M5 3H3v4h2V5h14v14H5v-2H3v4h18V3zm12 8h-2V9h-2V7h-2v2h2v2H3v2h10v2h-2v2h2v-2h2v-2h2z"></path>
+				</svg>
+			) : (
+				<svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24">
+					<path fill="currentColor" d="M2 3h20v18H2zm18 16V7H4v12z"></path>
+				</svg>
+			)}
+			<span>{data?.session ? 'Dashboard' : 'Sign In'}</span>
+		</Button>
 	)
 }
